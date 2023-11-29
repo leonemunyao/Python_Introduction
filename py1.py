3489834dbfee456814b6c97bb1c8e00eda1ed51b
leonemunyao <leonemunyao@gmail.com>
2023-11-29 22:36:51 +0300

    Whetting the appetite

---
+++ py1.py
@@ -0,0 +1,12 @@
+# Python has efficient high-level data structures and a simple but effective approach to object oriented programming. The python interpreter and the extensive standard library are freely available. 
+# Lets start checking on python and how it works. We can start by;
+# 
+
+# WHETTING YOUR APPETITE
+# If you are a professional software developer, you may have to work with several C/C++/Java Libraries but find the usual write/compile/test/re-compile cyle is too slow. Python is just a language for you. You could
+# write a unix shell script 
+
+# USING PYTHON INTERPRETER
+# Python interpreter is usually installed in as usr/local/bin/python3.4 in a unix shell path. On a windows machine, the python installation is usually placed in C:/Python34. To exit the interpreter type, quit(). 
+# The interpreter line editing features include interactive editiing, history substitution and code completion on systems that support readline. The quickest way to see whether the command line editing is supported
+# is using 
